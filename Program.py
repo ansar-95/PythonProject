632e77ab8e7da1ff2cb2a60b2f7a56000d2e12e8
Ansar <moha.ans@hotmail.com>
2021-09-29 17:22:25 +0200

    add funtion tourner a d

--- Program.py
+++ Program.py
@@ -13,15 +13,22 @@ class Program:
         self.create_widgets()
         self.frame.pack(expand=YES,side=LEFT)
         self.dicimg = {}
+        self.listeFichier = []
     def create_widgets(self):
         self.create_source_dossier_button()
+        self.create_tourner_a_droite_button()
 
     def create_source_dossier_button(self):
         yt_button = Button(self.frame, text="Choisir un dossier",command=self.choisirUnDossier)
         yt_button.pack(pady=25, fill=X)
 
+    def create_tourner_a_droite_button(self):
+        yt_buttonDroite = Button(self.frame,text='Tourner a droite',command=lambda: self.tournerImageDroite(self._srcFolder,self.listeFichier))
+        yt_buttonDroite.pack(pady=25, fill=X)
+
     def choisirUnDossier(self):
         self._srcFolder = tkFileDialog.askdirectory(parent=self._fenetre,initialdir="/user",title='Please select a directory')
+        self.listeFichier = os.listdir(self._srcFolder)
         self.afficherLesImages(self._srcFolder,os.listdir(self._srcFolder))
 
     def afficherLesImages(self,src,listeFichier):
@@ -37,4 +44,12 @@ class Program:
             v["cadre%d" % p].create_image(0,0,anchor=NW,image =photo)
             v["cadre%d" % p].pack()
             p = p +1
+    
+    def tournerImageDroite(self,scr,listeFichier):
+
+        for i in listeFichier:
+            img = scr+"/"+i
+            image = Image.open(img) 
+            image.rotate(45)
         
+        self.afficherLesImages(scr,listeFichier)
